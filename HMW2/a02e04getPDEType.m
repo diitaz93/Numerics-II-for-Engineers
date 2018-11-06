070c9b328ffb9da50b8416c4815ab5b54b87bc10
diitaz93 <juansdb93@gmail.com>
2018-11-06 14:25:21 +0100

    finished version of the HMW code, with comments

--- HMW2/a02e04getPDEType.m
+++ HMW2/a02e04getPDEType.m
@@ -5,17 +5,25 @@ function [t] = a02e04getPDEType(A, b)
 % second order coefficient matrix A and the first order coefficient vector
 % b. t takes the value 1 for eliptic PDEs, 2 for parabolic PDEs, 3 for
 % hyperbolic PDEs, and 4 for unclassified PDEs.
-%   The function uses the eigenvalues...
+%   The function uses the eigenvalues' signs and the rank of [AB] to
+%   classify the PDE system.
 tic
-ev = eig(A);
-T = [A,b];
-s = sign(ev);
-n = size(A(:,1));
-if (nnz(s)==0 && length(unique(s))==1)
+ev = eig(A); % Eigenvalues of A
+T = [A,b]; % Concatenation of matrix A and vector b
+s = sign(ev); % Vector of signs of eigenvalues
+n = length(A(:,1)); % Dimension of matrix A
+% If statement for elliptic 
+% No non-zero elements and all of the same sign
+if (nnz(s)==n && length(unique(s))==1) 
     t = 1;
-elseif (nnz(s)==0 && (sum(s==1)==(n-1) || sum(s==-1)==(n-1)) )
+% If statement for hyperbolic
+% No non-zero elements and n-1 eigenvalues of same sign
+elseif (nnz(s)==n && (sum(s==1)==(n-1) || sum(s==-1)==(n-1) ))
         t = 3;
-elseif (rank(T)~=0 && nnz(s)==1 && length(unique(s))==2)
+% If statement for parabolic
+% n-1 non-zero elements, rank of [AB] diferent to zero and all non zero
+% eigenvalues of the same sign
+elseif (rank(T)==n && nnz(s)==(n-1) && length(unique(s))==2)
     t = 2;
 else
     t = 0;
